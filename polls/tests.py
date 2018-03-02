3dd72150f5a84137da72603110decb5bd54ef3fd
Dimitri Merejkowsky <dimitri.merejkowsky@tanker.io>
2018-03-02 11:35:43 +0100

    Reduce code duplication

--- polls/tests.py
+++ polls/tests.py
@@ -46,17 +46,27 @@ def create_question(question_text, days):
     return Question.objects.create(question_text=question_text, pub_date=time)
 
 
+def get_latest_list(client):
+    response = client.get(reverse('polls:index'))
+    assert response.status_code == 200
+    return response.context['latest_question_list']
+
+
+def assert_question_list_equals(actual_questions, expected_texts):
+    assert len(actual_questions) == len(expected_texts)
+    for actual_question, expected_text in zip(actual_questions, expected_texts):
+        assert actual_question.question_text == expected_text
+
+
 class QuestionIndexViewTests(TestCase):
     def test_no_questions(self):
         """
         If no questions exist, an appropriate message is displayed.
         """
         response = self.client.get(reverse('polls:index'))
-        assert response.status_code == 200
         assert "No polls are available." in response.rendered_content
 
-        response = self.client.get(reverse('polls:index'))
-        latest_list = response.context['latest_question_list']
+        latest_list = get_latest_list(self.client)
         assert not latest_list
 
     def test_past_question(self):
@@ -65,10 +75,8 @@ class QuestionIndexViewTests(TestCase):
         index page.
         """
         create_question(question_text="Past question.", days=-30)
-        response = self.client.get(reverse('polls:index'))
-        latest_list = response.context['latest_question_list']
-        assert len(latest_list) == 1
-        assert latest_list[0].question_text == "Past question."
+        latest_list = get_latest_list(self.client)
+        assert_question_list_equals(latest_list, ["Past question."])
 
     def test_future_question(self):
         """
@@ -78,7 +86,7 @@ class QuestionIndexViewTests(TestCase):
         create_question(question_text="Future question.", days=30)
         response = self.client.get(reverse('polls:index'))
         assert "No polls are available." in response.rendered_content
-        latest_list = response.context['latest_question_list']
+        latest_list = get_latest_list(self.client)
         assert not latest_list
 
     def test_future_question_and_past_question(self):
@@ -88,10 +96,8 @@ class QuestionIndexViewTests(TestCase):
         """
         create_question(question_text="Past question.", days=-30)
         create_question(question_text="Future question.", days=30)
-        response = self.client.get(reverse('polls:index'))
-        latest_list = response.context['latest_question_list']
-        assert len(latest_list) == 1
-        assert latest_list[0].question_text == "Past question."
+        latest_list = get_latest_list(self.client)
+        assert_question_list_equals(latest_list, ["Past question."])
 
     def test_two_past_questions(self):
         """
@@ -99,8 +105,6 @@ class QuestionIndexViewTests(TestCase):
         """
         create_question(question_text="Past question 1.", days=-30)
         create_question(question_text="Past question 2.", days=-5)
-        response = self.client.get(reverse('polls:index'))
-        latest_list = response.context['latest_question_list']
-        assert len(latest_list) == 2
-        assert latest_list[0].question_text == "Past question 2."
-        assert latest_list[1].question_text == "Past question 1."
+        latest_list = get_latest_list(self.client)
+        expected_texts = ["Past question 2.", "Past question 1."]
+        assert_question_list_equals(latest_list, expected_texts)
